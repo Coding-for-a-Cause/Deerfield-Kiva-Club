e15fd02e37a72022c85f396d327a30a813e86da6
Brian Reich <reichman213@gmail.com>
2021-03-09 09:27:16 -0600

    Make home page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import { PageProps } from "gatsby"
-import React from "react"
-import Layout from "../templates/Layout"
+import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-export default function IndexRoute(props: PageProps) {
-  return (
-    <Layout>
-      <h1>Path</h1>
-      <p>{props.path}</p>
-    </Layout>
-  )
+import Layout from '../templates/Layout';
+
+
+const Home = () => {
+    // const pageName = location.pathname.match(/[A-z]{1,}[^#?\/\.]/gi); // Pull page name from url path.
+
+    return ( 
+        <Layout>
+            <div style={{ height: "2566px", /*paddingTop: document.querySelector("header").clientHeight + 25*/ }}>
+                <img src="/static/greco.jpg" style={{ width: "100%", maxHeight: "300px" }} /> {/* temp (placeholder) image */}
+                { /* 
+                    TODO:
+                      * Cover image (carousel?)
+                      * Brief information / introductions
+                      * what "we" do?
+                */}
+            </div>
+        </Layout>
+    );
 }
+ 
+export default Home;
