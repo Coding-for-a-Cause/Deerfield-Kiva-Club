032ac3d40052eb28737ddb1c2756ec92093d1f4f
i-am-zach <zachlefko@gmail.com>
2021-03-09 11:12:44 -0600

    Pulling markdown data

--- components/Leader.tsx
+++ components/Leader.tsx
@@ -1,18 +1,23 @@
-import React from "react"
+import React from 'react';
+import { Leader as LeaderType } from '../types';
 
-export default function LeaderComponent({ leader }) {
+type LeaderComponentProps = {
+  leader: LeaderType
+}
+
+export default function LeaderComponent({ leader }: LeaderComponentProps) {
   return (
     <div className="leader">
       <div className="picture">
-        {/* <GatsbyImage image={image} alt={props.leader.frontmatter.name} /> */}
+        <img src={leader.data.profilePicture} alt={leader.data.name} />
       </div>
       <div className="content">
         <h1>
-          {leader.name}, {leader.role}
+          {leader.data.name}, {leader.data.role}
         </h1>
-        <h2>{leader.leadershipYears}</h2>
-        <p dangerouslySetInnerHTML={{ __html: leader.html }}></p>
+        <h2>{leader.data.leadershipYears}</h2>
+        <p dangerouslySetInnerHTML={{ __html: leader.contents }}></p>
       </div>
     </div>
-  )
+  );
 }
--- pages/leadership.tsx
+++ pages/leadership.tsx
@@ -1,25 +1,51 @@
-import React from "react"
-import TitleLayout from "../templates/TitleLayout"
-import Leader from "../components/Leader"
+import React from 'react';
+import TitleLayout from '../templates/TitleLayout';
+import Leader from '../components/Leader';
+import { GetStaticProps } from 'next';
 
-const Leadership = () => {
-  let leaders = [
-    {
-      name: "Zack Greco",
-      role: "Co-President",
-      leadershipYears: "2020-2021",
-      html: "",
-    },
-  ]
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { Leader as LeaderType } from '../types';
+
+type LeadershipProps = {
+  leaders: [LeaderType];
+};
+
+const Leadership = ({ leaders }: LeadershipProps) => {
   return (
-    <TitleLayout title={"Deerfield High School KIVA Leadership Team"}>
+    <TitleLayout title={'Deerfield High School KIVA Leadership Team'}>
       <div className="pad">
         {leaders.map((leader, idx) => (
           <Leader key={idx} leader={leader} />
         ))}
       </div>
     </TitleLayout>
-  )
-}
+  );
+};
 
-export default Leadership
+export default Leadership;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const basedir = path.join(process.cwd(), 'data', 'leadership');
+  const files = fs.readdirSync(basedir, 'utf-8');
+
+  const leaders = files.map((filename) => {
+    const markdownWithMetadata = fs.readFileSync(
+      path.join(basedir, filename),
+      'utf-8',
+    );
+    const parsedMarkdown = matter(markdownWithMetadata);
+
+    return {
+      contents: parsedMarkdown.content,
+      data: parsedMarkdown.data,
+    };
+  });
+
+  return {
+    props: {
+      leaders,
+    },
+  };
+};
