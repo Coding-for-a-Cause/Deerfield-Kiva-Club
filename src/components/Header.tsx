dfea8ee2487d950c63a86a2d3bb791ac5bd8970a
i-am-zach <zachlefko@gmail.com>
2021-03-09 10:01:30 -0600

    Fixed merge

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar"
 const Header = () => {
   const [page, setPage] = useState("")
   useEffect(() => {
-    const pageName = location.pathname.match(/[A-z]{1,}[^#?\/\.]/gi) || [""]  // Pull page name from url path.
+    const pageName = location.pathname.match(/[A-z]{1,}[^#?\/\.]/gi) || [""] // Pull page name from url path.
     setPage(pageName[0])
     console.log(pageName[0])
   }, [page])
--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -4,28 +4,6 @@ import Footer from "../components/Footer"
 import Header from "../components/Header"
 import "../styles/styles.scss"
 
-<<<<<<< HEAD
-
-type LayoutProps = {
-    children?: React.ReactNode;
-    page?: string;
-}
-
-const Layout = ({ children, page }: LayoutProps) => {
-    if (!page && location.pathname != "/") {
-        page = location.pathname.match(/[A-z]{1,}[^#?\/\.]/gi)[0]; // Pull page name from url path.
-    }
-
-    return (
-        <>
-            <Header page={ page || "" } />
-            <main>
-                { children }
-            </main>
-            <Footer />
-        </>
-    );
-=======
 const Layout = ({ children }) => {
   return (
     <>
@@ -34,7 +12,6 @@ const Layout = ({ children }) => {
       <Footer />
     </>
   )
->>>>>>> main
 }
 
 export default Layout
