b42796fb6c48aea7e608ec5738b1d6a643a97b5f
i-am-zach <zachlefko@gmail.com>
2021-03-10 10:38:10 -0600

    Form changes for Netlify

--- components/EmailList.tsx
+++ components/EmailList.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import styles from "../styles/form.module.scss";
+import styles from '../styles/form.module.scss';
 
 export default function EmailList() {
   return (
     <form
-      name="join-us"
-      data-netlify="true"
+      name="Join Us"
       method="POST"
+      data-netlify="true"
       className={styles.form}
     >
+      <input type="hidden" name="form-name" value="Join Us" />
       <p className={styles.formTitle}>
         Sign up for the email list to get reminders of when meetings will happen
         and other KIVA events.
