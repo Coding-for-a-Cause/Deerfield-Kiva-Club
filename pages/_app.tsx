b2784e85fd3647f499ef0d896aef1186564d21a8
Brian Reich <reichman213@gmail.com>
2021-03-13 12:29:20 -0600

    import home styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/styles.scss"
 import "../styles/leadership.scss"
+import "../styles/home.scss";
 import { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
