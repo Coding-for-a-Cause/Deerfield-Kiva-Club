c68bfce9fa226fbfcf56e3ab6d342e439e392c9e
Brian Reich <reichman213@gmail.com>
2021-03-13 12:27:22 -0600

    Add cover image placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 
 import Layout from '../templates/Layout';
+import HeroCard from '../components/HeroCard';
 
 
 const Home = () => {
+
     // const pageName = location.pathname.match(/[A-z]{1,}[^#?\/\.]/gi); // Pull page name from url path.
 
     return ( 
         <Layout>
             <div style={{ height: "2566px", /*paddingTop: document.querySelector("header").clientHeight + 25*/ }}>
-                <img src="/static/greco.jpg" style={{ width: "100%", maxHeight: "300px" }} /> {/* temp (placeholder) image */}
-                { /* 
-                    TODO:
-                      * Cover image (carousel?)
-                      * Brief information / introductions
-                      * what "we" do?
-                */}
+                {/* Stock image -- set the image at this path to the cover image for the home page! */}
+                <img className="cover-image" src="/images/home-cover-photo.jpg" />
+                <HeroCard />
             </div>
         </Layout>
     );
