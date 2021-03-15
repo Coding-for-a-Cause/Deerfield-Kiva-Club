cb77dbebc536836c900427bd4d42d9e6d854a7ec
Brian Reich <reichman213@gmail.com>
2021-03-14 23:51:37 -0500

    import more stylesheets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/styles.scss"
 import "../styles/leadership.scss"
 import "../styles/home.scss";
+import "../styles/loans.scss";
+import "../styles/table.scss";
 import { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
